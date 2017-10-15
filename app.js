26aafb7c5b8491baa0175cef0506b6811858843c
“Richard Middleton” <Richard@richardmiddleton.me>
2017-10-15 15:18:08 +0100

    home route

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ const app = express();
 
 // Set view engine
 app.set('view engine', 'ejs');
+
+// Create home route
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(3000, () => {
+    console.log('app started on port 3000');
+});
