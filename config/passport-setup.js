ffd947be22f377f86b5f4e77e34920cd0d1d6b11
“Richard Middleton” <Richard@richardmiddleton.me>
2017-10-24 14:57:24 +0100

    check if user exists

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,7 +1,8 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
-const User = require('../models/user-model')
+const User = require('../models/user-model');
+
 passport.use(
     new GoogleStrategy({
         // options for strategy
@@ -9,12 +10,21 @@ passport.use(
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret
     }, (accessToken, refreshToken, profile, done) => {
-        // passport callback function
-        new User({
-            username: profile.displayName,
-            googleId: profile.id
-        }).save().then((newUser) => {
-            console.log(`new user created ${newUser}`);
+        // Check if user exists in database
+        User.findOne({googleId: profile.id}).then((currentUser) => {
+            if(currentUser) {
+                //already have the user
+                console.log(`user is ${currentUser}`);
+            }
+            else {
+                //if not make user
+                new User({
+                    username: profile.displayName,
+                    googleId: profile.id
+                }).save().then((newUser) => {
+                    console.log(`new user created ${newUser}`);
+                });
+            }
         });
     })
 );
